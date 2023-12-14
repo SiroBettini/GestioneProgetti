87b437a5ed820cdc4bf9f1f9bf8f77f6c0aea716
Siro Bettini <siro.bettini@CPT.local>
2023-12-14 10:30:29 +0100

    test users

---
+++ Allegati/mockupData.sql
@@ -0,0 +1,4 @@
+USE gestione_progetti;
+INSERT INTO user (name,surname,email.phoneNumber,role,password) VALUES("Mauro","SAtest","sa.test@gmail.com","+41761234567","superadmin","Password&1");
+INSERT INTO user (name,surname,email.phoneNumber,role,password) VALUES("Carlo","Atest","a.test@gmail.com","+41761234567","admin","Password&1");
+INSERT INTO user (name,surname,email.phoneNumber,role,password) VALUES("Andrea","Ctest","c.test@gmail.com","+41761234567","contributor","Password&1");
