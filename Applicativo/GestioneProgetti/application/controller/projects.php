af27dce0aa7aa9f51b72aa74211503ecae0234b8
SiroBettini <siro.bettini@samtrevano.ch>
2023-12-19 20:56:09 +0100

    merge fix

--- Applicativo/GestioneProgetti/application/controller/projects.php
+++ Applicativo/GestioneProgetti/application/controller/projects.php
@@ -2,10 +2,13 @@
 class Projects
 {
     private $pm;
+    private $um;
     public function __construct()
     {
         require_once 'application/models/projectMapper.php';
+        require_once 'application/models/userMapper.php';
         $this->pm = new ProjectMapper();
+        $this->um = new UserMapper();
     }
 
     public function index()
@@ -23,18 +26,23 @@ class Projects
         header("location:" . URL . "projects");
     }
     public function new(){
+        $users = $this->um->fetch();
         require "application/views/components/header.php";
         require "application/views/components/navbar.php";
         require "application/views/addProject/addProject.php";
     }
 
     public function add(){
-        $title = $_POST[""];
-        $desc = $_POST[""];
-        $start = $_POST[""];
-        $archived = $_POST[""];
-        $contributor = $_POST[""];
-        $this->pm->addProject($title,$desc,$start,$archived,$contributor);
+        if(isset($_POST["title"]) && isset($_POST["desc"])){
+            $title = $_POST["title"];
+            $desc = $_POST["desc"];
+            $start = $_POST["start"];
+            $contributor = $_POST["usr"];
+        }else{
+            header("location:" . URL . "projects/new");
+            return;
+        }
+        $this->pm->addProject($title,$desc,$start,$contributor);
         header("location:" . URL . "projects");
     }
 }
--- Applicativo/GestioneProgetti/application/views/addProject/addProject.php
+++ Applicativo/GestioneProgetti/application/views/addProject/addProject.php
@@ -3,10 +3,20 @@
         <form method="POST" action="<?php echo URL ?>projects/add">
             <label for="user">Titolo</label>
             <input type="text" name="title" id="title" class="form-control">
-            <label for="user">Descrizione</label>
-            <input type="password" name="desc" id="desc" class="form-control">
-            <label for="user">Descrizione</label>
-            <input type="password" name="desc" id="desc" class="form-control">
+
+            <label for="desc">Descrizione</label>
+            <input type="text" name="desc" id="desc" class="form-control">
+
+            <label for="start">Data di inizio</label>
+            <input type="date" name="start" id="start" class="form-control">
+
+            <label for="usr">Collaboratore</label>
+            <select name="usr" id="usr" class="form-control">
+                <?php foreach($users as $usr):?>
+                <option value="<?php echo $usr->getId()?>"><?php echo $usr->getName() . " " . $usr->getSurname()?></option>
+                <?php endforeach;?>
+            </select>
+
             <br>
             <input type="submit" class="btn btn-primary" name="login">
         </form>
